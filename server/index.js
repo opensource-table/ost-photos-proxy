d11d93ea9dceade9d693bd66dc67b6ab87dd4a3e
Wayne Sun <wtse.sun@gmail.com>
2019-06-14 11:05:09 -0700

    installed http proxy middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const proxy = require('http-proxy-middleware');
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
